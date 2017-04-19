9341c19a45ed0aa859d824c0e2abf7c2f857df88
pamzer156 <1035465284@qq.com>
2017-04-19 11:48:06 +0800

    'mime'

---
+++ 2.http/2.server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var MIME = {
+    '.css':'text/css',
+    '.html':'text/html',
+    '.js':'application/javascript',
+    '.ico':'application/x-ico'
+};
+http.createServer(function (req,res) {
+    let {pathname,query} = url.parse(req.url,true);
+    if(pathname == '/'){
+        res.setHeader('Content-Type','text/html;charset=utf-8');
+        var result = fs.readFileSync('./index.html');
+        return res.end(result);
+    }
+    // /public/css/index.min.css
+   /* let index = pathname.lastIndexOf(".");
+    let attr = pathname.substr(index);*/
+    let attr =/(\.[a-zA-Z]+)$/.exec(pathname)[1];
+    res.setHeader('Content-Type',''+MIME[attr]+';charset=utf-8');
+    var result = fs.readFileSync('.'+pathname);
+    res.end(result);
+}).listen(8080,function () {
+    console.log('start 8080');
+});
