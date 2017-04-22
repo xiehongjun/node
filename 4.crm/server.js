4707a4516eb97f468f12142c3c7f02d050b9d9e7
pamzer156 <1035465284@qq.com>
2017-04-22 12:36:54 +0800

    index.html

--- 4.crm/server.js
+++ 4.crm/server.js
@@ -6,10 +6,12 @@
 
 //1.创建服务端 创建http服务，node提供了一些模块(核心模块) http 第三方模块
 let http = require('http');//引用http模块
+let fs = require('fs');
 let listener = function (req,res) { //浏览器访问服务端,就会执行这个函数，服务端会一直等待着客户端的到来
     //还要告诉客户端响应的内容是utf8格式
-    res.setHeader('Content-Type','text/plain;charset=utf-8');
-    res.end('你好');//字符串和buffer类型
+    let result = fs.readFileSync('./index.html');
+    res.setHeader('Content-Type','text/html;charset=utf-8');
+    res.end(result);//字符串和buffer类型
 };
 http.createServer(listener).listen(3000); //启动一个服务，ip默认本机localhost  访问通过ip+端口号 localhost:3000
 //favicon.ico 这个是浏览器默认请求的图标，看心情请求
