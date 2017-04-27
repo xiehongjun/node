882038deb88dd8481c1a070b8911fa41b5192fee
pamzer156 <1035465284@qq.com>
2017-04-27 17:47:45 +0800

    实现修改

--- vue/secondday/todo.js
+++ vue/secondday/todo.js
@@ -8,8 +8,16 @@ var vm = new Vue({
             {title:'大后天学react',isChecked:true}
         ],
         editTodo:'',
+        state:'',
+        currentTodo:''//当前点的是谁
     },
     methods:{
+        reset(){ //让当前点击的变成空
+            this.currentTodo = '';
+        },
+        changeTitle(todo){
+            this.currentTodo = todo;
+        },
         addTodo(){
             this.todos.push({title:this.editTodo,isChecked:false});
             this.editTodo = ''; //添加后清空输入框
@@ -37,6 +45,21 @@ var vm = new Vue({
         }*/
     },
     computed:{
+        cloneTodo(){
+            if(this.state == 'complete'){ //全部 返回所有数据
+                return this.todos;
+            }
+            if(this.state == 'finish'){  //完成 返回checked为true的数据
+                return this.todos.filter(function (item) {
+                    return item.isChecked;
+                })
+            }
+            if(this.state == 'unfinish'){//未完成 返回checked为false的数据
+                return this.todos.filter(function (item) {
+                    return !item.isChecked;
+                })
+            }
+        },
         total(){ //默认是get
             return this.todos.filter(function (item) {
                 return !item.isChecked;//表示将当前项isChecked为true的都删除
@@ -44,3 +67,11 @@ var vm = new Vue({
         }
     }
 });
+//hash值 可以在当前页面上 保证不跳转页面，并且更新url
+//获取hash值
+var listener = function () {
+    //当hash值变化时会执行此函数 将当前的hash值赋予给state
+    vm.state = window.location.hash.slice(1) || 'complete';
+};
+listener();
+window.addEventListener('hashchange',listener,false);
